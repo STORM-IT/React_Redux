cd37b874aae138f66b069d8c84577c55eb14c898
storm <stooormix@gmail.com>
2022-01-12 23:56:56 +0330

    start create person Managar Redux

--- src/Component/Redux/Store/store.js
+++ src/Component/Redux/Store/store.js
@@ -1,5 +1,12 @@
-import { createStore } from "redux";
+import { createStore , applyMiddleware,compose} from "redux";
 import {reducers} from './../Reducer'
+import thunk from "redux-thunk";
 
-export const store =createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export const store =createStore(
+    reducers,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    )
 store.subscribe(() => console.log(store.getState()));
